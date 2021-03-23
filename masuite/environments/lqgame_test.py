d5620a829f8f1cd6e5b2e030c8103a530768b13b
zanedma <zanedma@gmail.com>
2021-03-23 13:43:35 -0700

    lqgame_test.py working

--- masuite/environments/lqgame_test.py
+++ masuite/environments/lqgame_test.py
@@ -2,10 +2,13 @@ import torch
 from lqgame import LinearQuadraticGame
 from masuite.utils.envutils import scalar_zerosum_game
 
-def step_game(**params):
-    game = LinearQuadraticGame(**params)
-    return game.step(torch.FloatTensor([[1.]]))
+def step_game(game):
+    return game.step(torch.FloatTensor([[1.], [1.]]))
 
 def test_step():
-    params = scalar_zerosum_game()
-    assert step_game(**params) == 0
+    game = scalar_zerosum_game()
+    ret = step_game(game)
+    assert isinstance(ret[0], torch.Tensor)
+    assert isinstance(ret[1], torch.Tensor)
+    assert isinstance(ret[2], bool)
+    assert isinstance(ret[3], dict)
