f5adc910f4d35a0094560e95a61cf0b2c98d2976
Ben Chasnov <bchasnov@uw.edu>
2021-03-23 14:11:37 -0700

    Create agent.py

---
+++ masuite/baselines/pytorch/policy_gradient/agent.py
@@ -0,0 +1,4 @@
+"""A simple multi-agent policy gradient implementation in PyTorch."""
+
+from bsuite.baselines.jax.policy_gradient.agent import PolicyGradient
+from bsuite.baselines.jax.policy_gradient.agent import default_agent
