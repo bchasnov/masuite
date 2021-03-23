3ef49437ec40dba0fe4c7372fca7f11ac00fc192
Ben Chasnov <bchasnov@uw.edu>
2021-03-23 14:52:20 -0700

    Update run.py

--- masuite/baselines/pytorch/policy_gradient/run.py
+++ masuite/baselines/pytorch/policy_gradient/run.py
@@ -1,11 +1,10 @@
 """Run a policy gradient agent instance on a masuite experiment."""
 from masuite.baselines import experiment
 from masuite.baselines.pytorch import policy_gradient
-from masuite.utils import dict2config
 
 def run(CONFIG):
   env = masuite.load_and_record(
-    env_id=env_id,
+    env_name=env_name,
     save_path=CONFIG.save_path,
     logging_mode=CONFIG.logging_mode,
   )
@@ -13,27 +12,27 @@ def run(CONFIG):
   agents = []
   for i in range(env.n_players):
     agents.append(
-      policy_gradient.default_agent(env.observation_spec(), env.action_spec())
+      policy_gradient.default_agent(env.observation_spec(), env.action_spec(), lr=CONFIG.lr)
     )
-  
-  alg = masuite.algorithm(
-    alg_id=alg_id,
-    agents=agents,
-    save_path=CONFIG.save_path,
-    logging_mode=CONFIG.logging_mode,
-  )
   
   experiment.run(
     agents=agents,
     environment=env,
-    algorithm=alg,
     num_episodes=CONFIG.num_episodes, 
     verbose=CONFIG.verbose)
 
-
-def main():
+def main(CONFIG):
   run(CONFIG)
 
 if __name__ == '__main__':
-  CONFIG = dict2config(env_id='lqgame-2p-zs', alg_id='simgrad', save_path='', num_episodes=int(1e4), verbose=true)
-  main()
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
+  parser.add_argument('--render', action='store_true')
+  parser.add_argument('--lr', type=float, default=1e-2)
+  parser.add_argument('--save_path', type=float, default=1e-2)
+  parser.add_argument('--num_episodes', type=float, default=int(1e3))
+  parser.add_argument('--verbose', action='store_true')
+  
+  CONFIG = parser.parse_args()    import argparse
+  main(CONFIG)
