c15475fffaefe625e17a307490a0272c358947c3
zanedma <zanedma@gmail.com>
2021-04-28 12:31:29 -0700

    first render implementation

--- masuite/environments/cartpole2p.py
+++ masuite/environments/cartpole2p.py
@@ -42,8 +42,83 @@ class Cartpole2PEnv(Environment):
     def reset(self):
         obs = [self.envs[i].reset() for i in range(self.n_players)]
         return obs
+    
+
+    def render(self, mode='human'):
+        screen_width = 600
+        screen_height = 400
+
+        world_width = self.x_threshold * 2
+        scale = screen_width/world_width
+        carty = 100  # TOP OF CART
+        polewidth = 10.0
+        polelen = scale * (2 * self.length)
+        cartwidth = 50.0
+        cartheight = 30.0
+
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+            axleoffset = cartheight / 4.0
+            cart1 = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            cart2 = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            self.carttrans1 = rendering.Transform()
+            self.carttrans2 = rendering.Transform()
+            cart1.add_attr(self.carttrans1)
+            cart2.add_attr(self.carttrans2)
+            self.viewer.add_geom(cart1)
+            self.viewer.add_geom(cart2)
+            l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
+            pole1 = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            pole2 = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            pole1.set_color(.8, .6, .4)
+            pole2.set_color(.8, .6, .4)
+            self.poletrans1 = rendering.Transform(translation=(0, axleoffset))
+            self.poletrans2 = rendering.Transform(translation=(0, axleoffset))
+            pole1.add_attr(self.poletrans1)
+            pole1.add_attr(self.carttrans1)
+            pole2.add_attr(self.poletrans2)
+            pole2.add_attr(self.carttrans2)
+            self.viewer.add_geom(pole1)
+            self.viewer.add_geom(pole2)
+            self.axle1 = rendering.make_circle(polewidth/2)
+            self.axle2 = rendering.make_circle(polewidth/2)
+            self.axle1.add_attr(self.poletrans1)
+            self.axle1.add_attr(self.carttrans1)
+            self.axle2.add_attr(self.poletrans2)
+            self.axle2.add_attr(self.carttrans2)
+            self.axle1.set_color(.5, .5, .5)
+            self.axle2.set_color(.5, .5, .5)
+            self.viewer.add_geom(self.axle1)
+            self.viewer.add_geom(self.axle2)
+            self.track = rendering.Line((0, carty), (screen_width, carty))
+            self.track.set_color(0, 0, 0)
+            self.viewer.add_geom(self.track)
+
+            self._pole_geoms = [pole1, pole2]
+
+            if self.envs[0].state is None or self.envs[1].state is None:
+                return None
+            
+            pole1, pole2 = self._pole_geoms[0], self._pole_geoms[1]
+            l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
+            pol1.v = [(l, b), (l, t), (r, t), (r, b)]
+            pol2.v = [(l, b), (l, t), (r, t), (r, b)]
+
+            x1, x2 = self.envs[0].state, self.envs[1].state
+            cart1x = x1[0] * scale + screen_width / 2
+            cart2x = x2[0] * scale + screen_width / 2
+            self.carttrans1.set_translation(cart1x, carty)
+            self.carttrans2.set_translation(cart2x, carty)
+            self.poletrans1.set_rotation(-x1[2])
+            self.poletrans2.set_rotation(-x2[2])
+
+            return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+            
 
 if __name__ == '__main__':
     env = Cartpole2PEnv(0, 5000)
     print(env.reset())
     print(env.step(acts=[0, 1]))
+    env.render()
